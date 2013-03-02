3105e66689af3abb6d71751cfc396ba085c8fd36
TheCJGCJG <charlesgillham@me.com>
2013-03-02 23:15:00 +0000

    Update Command_listsync.java
    Added New Message

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_listsync.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_listsync.java
@@ -16,7 +16,7 @@ public class Command_listsync extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.adminAction(sender.getName(), "Downloading superadmin and permban lists from primary CJGFreedom server.", false);
+        TFM_Util.adminAction(sender.getName(), "Downloading superadmin and permban lists from http://www.thecjgcjg.com.", false);
 
         try
         {
